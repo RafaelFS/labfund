0b9695e62422294f458e60401ef78e69d64bbf84
RafaelFS <rfl.fsilva@gmail.com>
2014-09-16 14:16:55 -0300

    A02E03 properly working. However, it does not find out invalid operations

--- aula2/T3G11A02E03.asm
+++ aula2/T3G11A02E03.asm
@@ -6,8 +6,17 @@
 			@ 		/0000		; Avisa o montador que o endereço inicial do programa é 0000
 INI			JP		MAIN		; Pula para o início do programa MAIN
 NWORDS		K		/0000		; Número de palavras a serem copiadas
-ENDORIGEM	K		/5000		; Endereço inicial da sequência de origem
-ENDDESTINO	K		/7000		; Endereço inicial da sequência de destino
+ENDORIGEM	K		/0008		; Endereço inicial da sequência de origem
+ENDDESTINO	K		/0012		; Endereço inicial da sequência de destino
+SEQORIGEM	K 		/0001		; Inicio dos endereços reservados para a origem
+			K		/0002		
+			K 		/0003
+			K		/0004		
+			@		/0010		; Seto os 4 endereços a partir do 10 com FFFF para melhorar visualização dos testes
+			K		/FFFF
+			K		/FFFF
+			K		/FFFF
+			K		/FFFF
 			@		/0030		; Reservo o espaço até o endereço 28, avisando que a próxima instrução ocorre no endereço 30
 ;Corpo do programa principal
 MAIN		LD 		NWORDS		; Copia o conteúdo de NWORDS para o Acumulador
@@ -29,8 +38,11 @@ SDESATUAL	K		/0000		; Endereço atual para onde estou copiando
 CONTAGEM	K		/0000		; Número de palavras que ainda preciso copiar
 OK			K		/0000		; Código de retorno de funcionamento correto
 ERRO 		K		/FFFF		; Código de retorno de caso de erro 
-MMVAZIA		MM      /0000
-LDVAZIA		MM      /0000
+MMVAZIA		MM      /0000       ; Código com MM vazia
+LDVAZIA		LD    	/0000		; Código com LD vazia
+COPIANDO 	K		/0000		; Dado que está sendo copiado
+INCADDRESS	K		/0002		; Constante de incremento de endereço
+DECCONT		K 		/0001		; Constante de decremento de contagem
 
 ;Corpo da Subrotina
 MEMCOPY		K 		/0000		; Início da subrotina MEMCOPY (endereço reservado para retorno)
@@ -41,7 +53,29 @@ MEMCOPY		K 		/0000		; Início da subrotina MEMCOPY (endereço reservado para ret
 			MM 		SDESATUAL   ; Copio o endereço de destino para endereço que estou lendo
 			LD      SNWORDS     ; Copio o número de palavras para o Acumulador
 			MM 		CONTAGEM    ; Copio o número de palavras para CONTAGEM
+			; Loop de copias
 LOOP 		JZ		FIMMEMCOPY  ; Pula para fora do LOOP se CONTAGEM = 0
+			LD 		SORIATUAL   ; Copio o endereço atual a ser lido para o Acumulador
+			+		LDVAZIA     ; Somo o endeço atual a ser lido com a instrução de LOAD, obtendo uma instrução que lê o valor atual
+			MM		COPIAATUAL 	; Envio a instrução que lê o valor atual para COPIAATUAL
+COPIAATUAL	K 		/0000		; Executa instrução que lê o valor atual
+			MM 		COPIANDO    ; Copia o valor atual para COPIANDO
+			LD 		SDESATUAL   ; Copio o endereço atual de destino para o Acumulador
+			+		MMVAZIA     ; Somo o endereço atual de destino com a instrução de MM, obtendo uma instrução que envia para o destino atual
+			MM		ENVIAATUAL 	; Envio a instrução que envia para o destino atual para ENVIAATUAL
+			LD 		COPIANDO    ; Copio o valor atual para o Acumulador
+ENVIAATUAL	K 		/0000		; Executa instrução que envia para o destino atual
+			;Incrementando os endereços e decrementando a CONTAGEM
+			LD 		SORIATUAL   
+			+ 		INCADDRESS
+			MM      SORIATUAL
+			LD 		SDESATUAL   
+			+ 		INCADDRESS
+			MM      SDESATUAL
+			LD 		CONTAGEM
+			- 		DECCONT
+			MM 		CONTAGEM
+			JP		LOOP 		; Retorna ao Inicio do LOOP 
 FIMMEMCOPY	LD 		OK			; Coloco o código de OK no Acumulador
 			RS 		MEMCOPY		; Retorno da subrotina
 			#		INI
