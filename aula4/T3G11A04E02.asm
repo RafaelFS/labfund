8fc60224ab4501cf12a38a8df4dcd9077dacca77
RafaelFS <rfl.fsilva@gmail.com>
2014-09-29 13:56:03 -0300

    OS now properly puts return value in Acumulator

--- aula4/T3G11A04E02.asm
+++ aula4/T3G11A04E02.asm
@@ -1,7 +1,7 @@
 						@ 		/0000				; Inicio do programa em 0000
 INI						JP 		MAIN_OS				; Pula para a label MAIN_PACK
 LU						K 		/0002				; Parâmetro de unidade lógica para a função de adicionar dispostivo
-TYPE					K		/0004				; Parâmetro de tipo de dispostivo para a função de adicionar dispostivo
+TYPE					K		/0003				; Parâmetro de tipo de dispostivo para a função de adicionar dispostivo
 MAIN_OS					OS 		/02AD				; Instrução OS que chama a função de adicionar dispostivo 
 FIM						HM 		FIM					; FIM
 						# 		INI					; Indica o fim do programa (Pseudo-comando)
