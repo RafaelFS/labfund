59dfa802d5a4e7729c9f626181ff916c996aeb50
RafaelFS <rfl.fsilva@gmail.com>
2014-10-06 20:19:35 -0300

    Running tests on A05E02 - not running after resetting

--- aula5/MVN/T3G11A05E02.asm
+++ aula5/MVN/T3G11A05E02.asm
@@ -1,6 +1,10 @@
 						@ 		/0000				; Inicio do programa em 0000
-INI						JP 		reset				; Pula para a label MAIN_PACK
+INI						JP 		RESET				; Pula para a label MAIN_PACK
 LU 						K 		/0000
-reset  					OS 		/01FA
+RESET 					OS 		/01FA
+						JP  	SKIP
+NUM_SKIP 				K 		/0002
+LU_SKIP					K  		/0000
+SKIP 				 	OS 		/02FE
 FIM						HM 		FIM					; FIM
 						# 		INI					; Indica o fim do programa (Pseudo-comando)
