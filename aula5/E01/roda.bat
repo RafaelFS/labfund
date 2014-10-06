3554cd70bdd8ce9a057c02de0af9b153b123df5c
RafaelFS <rfl.fsilva@gmail.com>
2014-10-06 13:47:53 -0300

    A05E01 tests completed

--- aula5/E01/roda.bat
+++ aula5/E01/roda.bat
@@ -1,6 +1,6 @@
-java -cp MLR.jar montador.MvnAsm T3G11A05E01_MAIN_12.asm
-java -cp MLR.jar montador.MvnAsm T3G11A05E01_rotinas_12.asm
-java -cp MLR.jar montador.MvnAsm T3G11A05E01_const_12.asm
-java -cp MLR.jar linker.MvnLinker T3G11A05E01_MAIN_12.mvn T3G11A05E01_rotinas_12.mvn T3G11A05E01_const_12.mvn -s pararelocar.mvn
+java -cp MLR.jar montador.MvnAsm T3G11A05E01_MAIN.asm
+java -cp MLR.jar montador.MvnAsm T3G11A05E01_rotinas.asm
+java -cp MLR.jar montador.MvnAsm T3G11A05E01_const.asm
+java -cp MLR.jar linker.MvnLinker T3G11A05E01_MAIN.mvn T3G11A05E01_rotinas.mvn T3G11A05E01_const.mvn -s pararelocar.mvn
 java -cp MLR.jar relocator.MvnRelocator pararelocar.mvn final.mvn 0000 0000
 java -jar MVN.jar
