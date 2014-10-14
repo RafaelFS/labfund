b88914ed5b7fb3e8cf18c753713443b44be9bc8c
unknown <t3m24@labsoft24.labsoft.local>
2014-10-14 16:15:38 -0300

    Started A07E02 - now main should dump in the proper UL

--- aula7/roda.bat
+++ aula7/roda.bat
@@ -1,6 +1,6 @@
-java -cp MLR.jar montador.MvnAsm T3G11A07E01_main.asm
-java -cp MLR.jar montador.MvnAsm T3G11A07E01_dumper.asm
-java -cp MLR.jar linker.MvnLinker T3G11A07E01_main.mvn T3G11A07E01_dumper.mvn  -s faltarelocar.mvn
+java -cp MLR.jar montador.MvnAsm T3G11A07E02_main.asm
+java -cp MLR.jar montador.MvnAsm T3G11A07E02_dumper.asm
+java -cp MLR.jar linker.MvnLinker T3G11A07E02_main.mvn T3G11A07E02_dumper.mvn  -s faltarelocar.mvn
 java -cp MLR.jar relocator.MvnRelocator faltarelocar.mvn final.mvn 0000 0000
 java -jar MVN.jar
 PAUSE
