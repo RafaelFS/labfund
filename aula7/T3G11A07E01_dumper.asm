f47b7140d4840bc9f184154b791f26f9e66c97a9
unknown <t3m24@labsoft24.labsoft.local>
2014-10-14 15:56:00 -0300

    A07E01 completed

--- aula7/T3G11A07E01_dumper.asm
+++ aula7/T3G11A07E01_dumper.asm
@@ -17,6 +17,26 @@ DUMP_BL 		K 		/0000			; Tamanho do bloco
 DUMP_EXE 		K 		/0000			; Endereço onde começaria a execução (valor dummy, apenas para manter o formato)
 DUMP_END_ATUAL	K 		/0000			; Endereço atual que será "dumpado"
 
+; Constantes usadas em DUMPER
+INC_ADDRESS		K		/0002		; Constante de incremento de endereço
+INVALID_ADDR	K		/1000       ; Constante de endereço invalido. Qualquer endereço igual ou maior que ele é inválido
+LD_VAZIA    	LD 		/0000		; Load vazia para criação de intruções dinamicamente
+
 
 DUMPER 			K 		/0000
-				LV 		DUMP_INI
+				LD 		DUMP_INI		; Pego o Endereço inicial e guardo como o endereço atual
+				MM 		DUMP_END_ATUAL
+DUMP_LOOP		LD		DUMP_END_ATUAL 	; Pego o Endereço atual e verifico se é igual ao endereço invalido. Se é igual, então acabou e vai para o final da Subrotina
+				-		INVALID_ADDR		
+				JZ 		DUMP_FIM		
+				LD		DUMP_END_ATUAL	; Se não acabou, pego o valor presente no endereço atual (com a ajuda de um load criado dinamicamente)
+				+		LD_VAZIA
+				MM 		DUMP_GET_VALOR
+DUMP_GET_VALOR	K		/0000			
+				PD		/300			; Salvo no disco
+				LD 		DUMP_END_ATUAL	; Incremento o endereço e volto ao início
+				+		INC_ADDRESS 	
+				MM 		DUMP_END_ATUAL
+				JP 		DUMP_LOOP
+DUMP_FIM		RS 		DUMPER
+				#		DUMPER
